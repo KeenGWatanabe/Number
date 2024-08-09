35d58e0fdc63c24d6fc94fd0346e40790e761500
60936 <studio34pl@gmail.com>
2024-08-09 18:10:22 +0800

    r

---
+++ integer/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        byte number=2;
+        Console.WriteLine(number);
+    }
+}
+// stop at 32:26
